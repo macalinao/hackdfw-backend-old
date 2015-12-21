d9e6419be1cc0e8da0e3cc5d36375402766b1ea0
Ian Macalinao <me@ian.pw>
2015-12-21 11:31:20 -0600

    fix: bad parenthesis

--- src/main/scala/com/hackdfw/backend/DB.scala
+++ src/main/scala/com/hackdfw/backend/DB.scala
@@ -3,6 +3,9 @@ package com.hackdfw.backend
 import slick.driver.PostgresDriver.api._
 
 object DB {
+
   val url = "jdbc:" + Option(System.getenv("HEROKU_POSTGRESQL_CRIMSON_URL")).getOrElse("postgres://localhost")
-  val connection = Database.forURL(url, driver = "org.postgresql.Driver"))
+
+  val connection = Database.forURL(url, driver = "org.postgresql.Driver")
+
 }
