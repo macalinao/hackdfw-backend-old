f4d701a124597d529ac1ae63c2ad37de1a3ec3ba
Ian Macalinao <me@ian.pw>
2015-12-21 11:28:07 -0600

    feat: add DB singleton

---
+++ src/main/scala/com/hackdfw/backend/DB.scala
@@ -0,0 +1,8 @@
+package com.hackdfw.backend
+
+import slick.driver.PostgresDriver.api._
+
+object DB {
+  val url = "jdbc:" + Option(System.getenv("HEROKU_POSTGRESQL_CRIMSON_URL")).getOrElse("postgres://localhost")
+  val connection = Database.forURL(url, driver = "org.postgresql.Driver"))
+}
