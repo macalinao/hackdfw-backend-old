afb7266957c0f9d256e446899731380dd0492c2b
Ian Macalinao <me@ian.pw>
2015-12-22 17:13:14 -0600

    feat: make password, confirmation_token, and confirmed_at optional

--- src/main/scala/com/hackdfw/backend/models/Users.scala
+++ src/main/scala/com/hackdfw/backend/models/Users.scala
@@ -5,17 +5,17 @@ import java.sql.Date
 import org.json4s.JsonAST.JValue
 import com.hackdfw.backend.MyPostgresDriver.api._
 
-class User(tag: Tag) extends Table[(Int, String, String, String, Date, JValue)](tag, "users") {
+class User(tag: Tag) extends Table[(Int, String, Option[String], Option[String], Option[Date], JValue)](tag, "users") {
 
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 
   def email = column[String]("email")
 
-  def password = column[String]("password")
+  def password = column[Option[String]]("password")
 
-  def confirmationToken = column[String]("confirmation_token")
+  def confirmationToken = column[Option[String]]("confirmation_token")
 
-  def confirmedAt = column[Date]("confirmed_at")
+  def confirmedAt = column[Option[Date]]("confirmed_at")
 
   def fields = column[JValue]("fields")
 
