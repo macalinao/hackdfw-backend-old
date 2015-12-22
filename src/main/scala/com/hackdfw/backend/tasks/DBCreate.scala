1b09df118c905e2d819c75319744900753cda9cd
Ian Macalinao <me@ian.pw>
2015-12-22 16:55:55 -0600

    feat: take as long as needed for DB creation

--- src/main/scala/com/hackdfw/backend/tasks/DBCreate.scala
+++ src/main/scala/com/hackdfw/backend/tasks/DBCreate.scala
@@ -3,20 +3,18 @@ package com.hackdfw.backend.tasks
 import com.hackdfw.backend.DB
 import com.hackdfw.backend.models
 import com.hackdfw.backend.MyPostgresDriver.api._
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-
-import scala.concurrent.Await
+import scala.concurrent.{ExecutionContext, Await}
 
 object DBCreate {
 
   def main(args: Array[String]): Unit = {
 
+    implicit val ec = ExecutionContext.global
     val result = DB.connection.run(DBIO.seq(
       models.schema.create
     ))
-
-    Await.result(result, 1 minute)
+    Await.result(result, Duration.Inf)
 
   }
 
